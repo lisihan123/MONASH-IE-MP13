98aa8a854c41fa6d7f814b59ea4df9e7bf99a683
Sihan <slii0093@student.monash.edu>
2020-08-25 02:03:57 +0800

    Simply modify home page, create a class extends animation to do card flip animation

---
+++ SaveTheBird2/app/src/main/java/com/example/savethebird/ui/home/HomeFragment.java
@@ -0,0 +1,39 @@
+package com.example.savethebird.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.savethebird.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+    private Button mtag1, mtag2;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        mtag1 = root.findViewById(R.id.btn_tag_1);
+        mtag2 = root.findViewById(R.id.btn_tag_2);
+        mtag1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return root;
+    }
+}
