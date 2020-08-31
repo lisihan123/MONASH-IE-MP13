19efacd95a0c4b1832a0c66c065c73cd92e48de5
Sihan <slii0093@student.monash.edu>
2020-08-31 17:32:50 +0800

    Updated

--- SaveTheBird2 5.06.45 pm/app/src/main/java/com/example/savethebird/MainActivity.java
+++ SaveTheBird2 5.06.45 pm/app/src/main/java/com/example/savethebird/MainActivity.java
@@ -27,7 +27,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-    private final static FactKidFragment fk = new FactKidFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,43 +35,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-//                .build();
-//
-//
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//        NavigationUI.setupWithNavController(navView, navController);
+         //menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
 
-//        Amplify.DataStore.observe(Todo.class,
-//                started -> Log.i("Tutorial", "Observation began."),
-//                change -> Log.i("Tutorial", change.item().toString()),
-//                failure -> Log.e("Tutorial", "Observation failed.", failure),
-//                () -> Log.i("Tutorial", "Observation complete.")
-//        );
-        navView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+        Amplify.DataStore.observe(Todo.class,
+                started -> Log.i("Tutorial", "Observation began."),
+                change -> Log.i("Tutorial", change.item().toString()),
+                failure -> Log.e("Tutorial", "Observation failed.", failure),
+                () -> Log.i("Tutorial", "Observation complete.")
+        );
+//        navView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
 
     }
 
 
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.navigation_home:
-                replace(new HomeFragment());
-                break;
-            case R.id.navigation_dashboard:
-                replace(new DashboardFragment());
-                break;
-            case R.id.navigation_notifications:
-                replace(new NotificationsFragment());
-                break;
-
-        }
-        return true;
-    }
+//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//        int id = item.getItemId();
+//        switch (id) {
+//            case R.id.navigation_home:
+//                replace(new HomeFragment());
+//                break;
+//            case R.id.navigation_dashboard:
+//                replace(new DashboardFragment());
+//                break;
+//            case R.id.navigation_notifications:
+//                replace(new NotificationsFragment());
+//                break;
+//
+//        }
+//        return true;
+//    }
 
     public void replace(Fragment nextFragment){
         getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, nextFragment).commit();
