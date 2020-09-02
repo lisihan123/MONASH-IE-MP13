0be38c20adce3860450b0911ac887e5ff7cbe8ee
Sihan <slii0093@student.monash.edu>
2020-09-02 21:59:04 +0800

    add package to load gif image

--- SaveTheBird2/app/src/main/java/com/example/savethebird/LoadingActiviy.java
+++ SaveTheBird2/app/src/main/java/com/example/savethebird/LoadingActiviy.java
@@ -1,8 +1,10 @@
 package com.example.savethebird;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -11,9 +13,20 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 
 public class LoadingActiviy extends AppCompatActivity {
-    private WebView mwbloading;
+    private ImageView megg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,54 +34,28 @@ public class LoadingActiviy extends AppCompatActivity {
         setContentView(R.layout.activity_loading_activiy);
 
         final Intent intent = new Intent(this,MainActivity.class);
-        mwbloading = (WebView) findViewById(R.id.wb_loading);
-        String url = "file:///android_asset/ebird.html";
-        mwbloading.getSettings().setJavaScriptEnabled(true);
-        //自适应屏幕
-        mwbloading.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-        mwbloading.getSettings().setLoadWithOverviewMode(true);
-        mwbloading.setWebViewClient(new WebViewClient() {
 
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                //该方法在Build.VERSION_CODES.LOLLIPOP以前有效，从Build.VERSION_CODES.LOLLIPOP起，建议使用shouldOverrideUrlLoading(WebView, WebResourceRequest)} instead
-                //返回false，意味着请求过程里，不管有多少次的跳转请求（即新的请求地址），均交给webView自己处理，这也是此方法的默认处理
-                //返回true，说明你自己想根据url，做新的跳转，比如在判断url符合条件的情况下，我想让webView加载http://ask.csdn.net/questions/178242
 
-                if (url.toString().contains("sina.cn")){
-                    view.loadUrl("http://ask.csdn.net/questions/178242");
-                    return true;
-                }
 
-                return false;
-            }
+        initView();
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request)
-            {
-                //返回false，意味着请求过程里，不管有多少次的跳转请求（即新的请求地址），均交给webView自己处理，这也是此方法的默认处理
-                //返回true，说明你自己想根据url，做新的跳转，比如在判断url符合条件的情况下，我想让webView加载http://ask.csdn.net/questions/178242
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    if (request.getUrl().toString().contains("sina.cn")){
-                        view.loadUrl("http://ask.csdn.net/questions/178242");
-                        return true;
-                    }
-                }
-
-                return false;
-            }
-
-        });
-        mwbloading.loadUrl(url);
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        startActivity(intent);
+//        startActivity(intent);
         //add finish method to close this activity
-        finish();
+//        finish();
 
+    }
+
+    private  void initView(){
+        megg = (ImageView) findViewById(R.id.imageView9);
+        load(megg);
+    }
+    private void load(ImageView image) {
+        String url = "";
+         Glide.with(LoadingActiviy.this).load(R.drawable.egg).diskCacheStrategy(DiskCacheStrategy.RESOURCE).into(image);
     }
 }
