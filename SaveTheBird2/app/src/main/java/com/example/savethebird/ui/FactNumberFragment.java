3ef77cb8ee23df315552df7c056a92ac68f8e74c
qshi0008 <qshi0008@student.monash.edu>
2020-09-03 00:53:32 +0800

    Modify Status Fact Page
    1.Review and reset the layout of "Status"fact page
    2.Rename the "fact_number" into "fact_status"

--- SaveTheBird2/app/src/main/java/com/example/savethebird/ui/FactNumberFragment.java
+++ SaveTheBird2/app/src/main/java/com/example/savethebird/ui/FactNumberFragment.java
@@ -21,7 +21,7 @@ public class FactNumberFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fact_number, container, false);
+        View rootView = inflater.inflate(R.layout.fact_status, container, false);
         return rootView;
 
     }
