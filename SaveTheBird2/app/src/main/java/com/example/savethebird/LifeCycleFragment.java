58495360b4a48c1c83670aed1dc17dcc796217b7
Sihan <slii0093@student.monash.edu>
2020-09-15 15:22:09 +0800

    Make image and textview suitable

--- SaveTheBird2/app/src/main/java/com/example/savethebird/LifeCycleFragment.java
+++ SaveTheBird2/app/src/main/java/com/example/savethebird/LifeCycleFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.savethebird.R;
 import com.example.savethebird.ui.home.MyAdapter;
@@ -20,6 +21,7 @@ public class LifeCycleFragment extends Fragment {
     List<Integer> lstImages = new ArrayList<>();
 
     private int position = 0;
+    TextView msg1,msg2,msg3,msg4;
 
     public LifeCycleFragment(int position){
         this.position = position;
@@ -33,21 +35,42 @@ public class LifeCycleFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.life_cycle, container, false);
-        initData();
+        initData(view);
         HorizontalInfiniteCycleViewPager pager = view.findViewById(R.id.horizontal_cycle);
         MyAdapter adapter = new MyAdapter(lstImages, getContext());
         pager.setAdapter(adapter);
         pager.setCurrentItem(position);
+        initText();
 
         return view;
     }
 
-    private void initData() {
+    private void initData(View view) {
         lstImages.add(R.drawable.lifecycle_bg_eggs);
         lstImages.add(R.drawable.lifecycle_bg_chick);
         lstImages.add(R.drawable.lifecycle_bg_juvenie);
         lstImages.add(R.drawable.lifecycle_bg_adult);
+        msg1 = view.findViewById(R.id.tv_stage_1);
+        msg2 = view.findViewById(R.id.tv_stage_2);
+        msg3 = view.findViewById(R.id.tv_stage_3);
+        msg4 = view.findViewById(R.id.tv_stage_4);
 
+
+    }
+
+    private void initText(){
+        if (position == 0){
+            msg1.setVisibility(View.VISIBLE);
+        }
+        if (position == 1){
+            msg2.setVisibility(View.VISIBLE);
+        }
+        if (position == 2){
+            msg3.setVisibility(View.VISIBLE);
+        }
+        if (position == 3){
+            msg4.setVisibility(View.VISIBLE);
+        }
     }
 
 }
