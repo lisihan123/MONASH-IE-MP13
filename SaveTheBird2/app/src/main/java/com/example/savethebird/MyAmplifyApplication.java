1d32a75316d95c0d30fa1b59ebcb745354c5b75e
Karen <lwuu0021@student.monash.edu>
2020-09-30 22:56:45 +1000

    Ampify changed

--- SaveTheBird2/app/src/main/java/com/example/savethebird/MyAmplifyApplication.java
+++ SaveTheBird2/app/src/main/java/com/example/savethebird/MyAmplifyApplication.java
@@ -3,8 +3,13 @@ package com.example.savethebird;
 import android.app.Application;
 import android.util.Log;
 
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.Callback;
+import com.amazonaws.mobile.client.UserStateDetails;
 import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.api.rest.RestOptions;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.AWSDataStorePlugin;
 
@@ -15,10 +20,43 @@ public class MyAmplifyApplication extends Application {
 
         try {
             // Add these lines to add the AWSApiPlugin plugins
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
             Amplify.addPlugin(new AWSApiPlugin());
             Amplify.configure(getApplicationContext());
 
-            Log.i("MyAmplifyApp", "Initialized Amplify");
+//            AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
+//
+//                        @Override
+//                        public void onResult(UserStateDetails userStateDetails) {
+//                            Log.i("INIT", "onResult: " + userStateDetails.getUserState());
+//                        }
+//
+//                        @Override
+//                        public void onError(Exception e) {
+//                            Log.e("INIT", "Initialization error.", e);
+//                        }
+//                    }
+//            );
+
+//            Log.i("MyAmplifyApp", "Initialized Amplify");
+//
+//            RestOptions options = RestOptions.builder()
+//                    .addPath("/todo")
+//                    .addBody("{\"name\":\"Mow the lawn\"}".getBytes())
+//                    .build();
+//
+//            Amplify.API.post(options,
+//                    response -> Log.i("MyAmplifyApp", "POST " + response.getData().asString()),
+//                    error -> Log.e("MyAmplifyApp", "POST failed", error)
+//            );
+//
+
+
+//            Amplify.Auth.fetchAuthSession(
+//                    result -> Log.i("AmplifyQuickstart", result.toString()),
+//                    error -> Log.e("AmplifyQuickstart", error.toString())
+//            );
+
         } catch (AmplifyException error) {
             Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
         }
