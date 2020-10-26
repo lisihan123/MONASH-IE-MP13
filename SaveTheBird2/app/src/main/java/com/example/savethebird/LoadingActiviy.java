758f96fc72e892fe65f44440332c430b648f20fb
Karen <lwuu0021@student.monash.edu>
2020-10-26 23:12:07 +1100

    Correct file name

--- SaveTheBird2/app/src/main/java/com/example/savethebird/LoadingActiviy.java
+++ SaveTheBird2/app/src/main/java/com/example/savethebird/LoadingActiviy.java
@@ -1,29 +1,14 @@
 package com.example.savethebird;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.Priority;
-import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.load.resource.gif.GifDrawable;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.target.Target;
 
 public class LoadingActiviy extends AppCompatActivity {
     private ImageView megg;
@@ -31,7 +16,7 @@ public class LoadingActiviy extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loading_activiy);
+        setContentView(R.layout.activity_loading_activity);
 
         final Intent intent = new Intent(this,MainActivity.class);
 
